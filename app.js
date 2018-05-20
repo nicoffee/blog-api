c3b4e67ec43476ceaa47a900a1df258521a28a45
nicoffee <fedoseevn@gmail.com>
2018-05-20 16:14:43 +0500

    Refactoring

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var index = require('./routes/index');
-var notes = require('./routes/notes');
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+const mongoose = require('mongoose');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const notesRouter = require('./routes/notes');
 
-app.use(cors({ credentials: true, origin: true }));
+const url = 'mongodb://localhost:27017';
+const dbName = 'test';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+mongoose.connect(`${url}/${dbName}`);
+
+const app = express();
+
+// app.use(cors({ credentials: true, origin: true }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/notes', notes);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const User = require('./../models/User');
+
+const router = express.Router();
+
+router.post('/', function(req, res, next) {
+  if (
+    req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf
+  ) {
+    const userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf
+    };
+
+    // kitty.save().then(() => console.log('meow'));
+
+    User.create(userData, function(err) {
+      if (err) {
+        return next(err);
+      }
+    });
+  }
+
+  res.send('respond with a resource');
+});
+
+module.exports = router;
